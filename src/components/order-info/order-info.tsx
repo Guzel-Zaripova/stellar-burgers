ab48d43bafb91d0156967be7df74fed134653d21
Guzel Zaripova <guzel.zhagfarovna@gmail.com>
2025-06-27 11:20:06 +0300

    Создание orderInfoSelector и заполнение OrderInfo

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,21 +1,24 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
 import { TIngredient } from '@utils-types';
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch } from 'src/services/store';
+import { getOrderByNumber, selectIngredients } from '@slices';
+import { orderInfoSelector } from '@selectors';
 
 export const OrderInfo: FC = () => {
-  /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const dispatch = useDispatch<AppDispatch>();
+  const number = useParams().number || '';
+  const ingredients = useSelector(selectIngredients);
+  const orderData = useSelector(orderInfoSelector(number));
 
-  const ingredients: TIngredient[] = [];
+  useEffect(() => {
+    if (!orderData) {
+      dispatch(getOrderByNumber(+number));
+    }
+  }, [dispatch, orderData, number]);
 
   /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
---
+++ src/services/selectors/index.ts
@@ -0,0 +1,21 @@
+import { RootState } from '../store';
+
+export const orderInfoSelector = (number: string) => (state: RootState) => {
+  if (state.profileOrders.order.length) {
+    const data = state.profileOrders.order.find(
+      (item) => item.number === +number
+    );
+    if (data) return data;
+  }
+
+  if (state.feed.orders.length) {
+    const data = state.feed.orders.find((item) => item.number === +number);
+    if (data) return data;
+  }
+
+  if (state.order.orderModalData?.number === +number) {
+    return state.order.orderModalData;
+  }
+
+  return null;
+};
